3a7e88cb8925ff887e5e104152ec1c8a49534ff6
정솔미 <solmi-@kookmin.ac.kr>
2025-01-21 12:02:30 +0900

    install cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
       },
       actionOptions: {
         upload: {},
