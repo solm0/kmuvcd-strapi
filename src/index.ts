c02b692dc558add441047d3b0ae31a03c2682bf2
정솔미 <solmi-@kookmin.ac.kr>
2025-01-25 22:10:38 +0900

    edit 'Can only assign Authors'

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 const condition = {
-  displayName: 'Can only assign Authors',
-  name: 'can-only-assign-authors',
+  displayName: 'Can only assign Authors', // Name shown in the Admin Panel
+  name: 'can-only-assign-authors',        // Unique condition name
   async handler(user) {
+    // Define the allowed role names
     const allowedAdminRoles = ['Author'];
-    return { role: { $in: allowedAdminRoles } };
+    // Filter by role.name instead of role
+    return { role: { name: { $in: allowedAdminRoles } } };
   },
 };
 
-export default {
+module.exports = {
   async bootstrap({ strapi }) {
+    // Register the condition
     await strapi.admin.services.permission.conditionProvider.register(condition);
   },
 };
