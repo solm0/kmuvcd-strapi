960910ced2fbc33532b90f4c6bbae120fcf375b1
정솔미 <solmi-@kookmin.ac.kr>
2025-01-25 21:41:42 +0900

    custom condition 'Can only assign Authors'

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-// import type { Core } from '@strapi/strapi';
+const condition = {
+  displayName: 'Can only assign Authors',
+  name: 'can-only-assign-authors',
+  async handler(user) {
+    const allowedAdminRoles = ['Author'];
+    return { role: { $in: allowedAdminRoles } };
+  },
+};
 
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
-
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }) {
+    await strapi.admin.services.permission.conditionProvider.register(condition);
+  },
 };
