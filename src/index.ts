86179db659d7f1c3c6ecb73b9d492fbe8368f44e
정솔미 <solmi-@kookmin.ac.kr>
2025-01-25 22:25:47 +0900

    edit 'Can only assign Authors'

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ const condition = {
   displayName: 'Can only assign Authors', // Name shown in the Admin Panel
   name: 'can-only-assign-authors',        // Unique condition name
   async handler(user) {
-    // Define the allowed role names
-    const allowedAdminRoles = ['Author'];
-    // Filter by role.name instead of role
-    return { role: { name: { $in: allowedAdminRoles } } };
+    const allowedAdminRoles = ['author'];
+    return { code: { $in: allowedAdminRoles } };
   },
 };
 
