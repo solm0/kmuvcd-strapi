160ffcbeb288f3e42e866758b4d0bcbd3978e0f3
정솔미 <solmi-@kookmin.ac.kr>
2025-02-04 14:16:06 +0900

    add url

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://kmuvcd-strapi.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
