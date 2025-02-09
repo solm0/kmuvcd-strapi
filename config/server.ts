9f63080d17506af09fef7cb077dab072decf4c36
정솔미 <solmi-@kookmin.ac.kr>
2025-02-09 21:33:11 +0900

    public url only in production

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://kmuvcd-strapi.onrender.com'),
+  url: env('NODE_ENV') === 'production' ? env('PUBLIC_URL', 'https://kmuvcd-strapi.onrender.com') : undefined,
   app: {
     keys: env.array('APP_KEYS'),
   },
